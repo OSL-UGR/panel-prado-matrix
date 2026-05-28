795c96e8dba86c56d63ff8ab1e1327643a0dbd88
Zapi24 <samueluca2004@gmail.com>
2026-05-28 12:32:39 +0200

    Definimos los modelos de las tablas usuarios y sala_asignaturas

--- backend/app/models/sala_asignaturas.py
+++ backend/app/models/sala_asignaturas.py
@@ -1,3 +1,28 @@
 # <- ¿De qué se encarga este archivo? -> 
 #
-# Este archivo es la representación en código de la tabla de usuarios
+# Este archivo es la representación en código de la tabla de sala_asingaturas
+
+from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy.ext.declarative import declarative_base # Crea la base para todos los modelos ORM
+
+Base = declarative_base()
+
+class TipoSala(enum.Enum):
+    espacio = "espacio"
+    sala = "sala"
+    sala_avisos = "sala_avisos"
+
+class SalaAsignatura(Base):
+
+    __tablename__ = "sala_asignaturas"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    # El index es para que las busquedas sean mas rapidas, renta ponerlo en aquellas tuplas que consultaremos más
+    id_asignatura_prado = Column(String, index=True) 
+
+    id_matrix_sala = Column(String, index=True) # Ej: !xyz123:matrix.ugr.es
+
+    alias_principal = Column(String)
+
+    tipo = Column(Enum(TipoSala)) # Espacio, sala o sala_avisos
--- backend/app/models/usuarios.py
+++ backend/app/models/usuarios.py
@@ -1,3 +1,21 @@
 # <- ¿De qué se encarga este archivo? -> 
 #
 # Este archivo es la representación en código de la tabla de usuarios
+
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base # Crea la base para todos los modelos ORM
+
+Base = declarative_base()
+
+class Usuario(Base):
+
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    correo = Column(String, unique=True)
+
+    nombre = Column(String)
+
+    # El index es para que las busquedas sean mas rapidas, renta ponerlo en aquellas tuplas que consultaremos más
+    matrix_id = Column(String, unique=True, index=True) # Ej: usuario:matrix.ugr.es
